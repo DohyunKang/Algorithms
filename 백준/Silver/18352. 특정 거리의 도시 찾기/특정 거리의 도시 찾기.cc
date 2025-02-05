24de578629477fa41043605e947605e64150dbf6
Kangdo <166014987+DohyunKang@users.noreply.github.com>
2025-02-05 09:59:27 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 344 ms, Memory: 21724 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -13,75 +13,77 @@ void BFS(int node);
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    int N,M,K,X;
-    
-    cin >> N >> M >> K >> X;
-    
-    A.resize(N+1);
-    int s,e;
-    
-    for(int i = 0; i < M; i++)
-    {
-        cin >> s >> e;
-        A[s].push_back(e);
-    }
-    
-    visited.resize(N+1);
-    
-    for(int i = 0; i <= N; i++)
-    {
-        visited[i] = -1;
-    }
-    
-    BFS(X);
-    
-    for(int i = 0; i <= N; i++)
-    {
-        if(visited[i] == K)
-        {
-            answer.push_back(i);
-        }
-    }
-    
-    if(answer.empty())
-    {
-        cout << -1 << "\n";
-    }
-    else
-    {
-        sort(answer.begin(),answer.end());
-        
-        for(int i = 0; i < answer.size(); i++)
-        {
-            cout << answer[i] << "\n";
-        }
-    }
-    
-    return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M, K, X;
+
+	cin >> N >> M >> K >> X;
+
+	A.resize(N + 1);
+	visited.resize(N + 1);
+
+	int s, e;
+
+	for (int i = 0; i < M; i++)
+	{
+		cin >> s >> e;
+
+		A[s].push_back(e);
+	}
+
+	for (int i = 0; i <= N; i++)
+	{
+		visited[i] = -1;
+	}
+
+	BFS(X);
+
+	for (int i = 0; i <= N; i++)
+	{
+		if (visited[i] == K)
+		{
+			answer.push_back(i);
+		}
+	}
+
+	if (answer.empty())
+	{
+		cout << -1 << "\n";
+	}
+	else
+	{
+		sort(answer.begin(),answer.end());
+
+		for (int i = 0; i < answer.size(); i++)
+		{
+			cout << answer[i] << "\n";
+		}
+
+	}
+
+	return 0;
 }
 
 void BFS(int node)
 {
-    queue<int> myqueue;
-    myqueue.push(node);
-    visited[node]++;
-    
-    while(!myqueue.empty())
-    {
-        int now_node = myqueue.front();
-        myqueue.pop();
-        
-        for(int i : A[now_node])
-        {
-            if(visited[i] == -1)
-            {
-                visited[i] = visited[now_node] + 1;
-                myqueue.push(i);
-            }
-        }
-    }
+	queue<int> myqueue;
+	myqueue.push(node);
+	visited[node]++;
+
+	while (!myqueue.empty())
+	{
+		int now_node = myqueue.front();
+		myqueue.pop();
+
+		for (int i : A[now_node])
+		{
+			if (visited[i] == -1)
+			{
+				visited[i] = visited[now_node] + 1;
+				myqueue.push(i);
+			}
+		}
+	}
 }
