19f3c60da1aad2af8b153f908b6b32cff3003564
Kangdo <166014987+DohyunKang@users.noreply.github.com>
2025-02-05 09:38:01 +0900

    [Gold IV] Title: 수 묶기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.cc
+++ 백준/Gold/1744. 수 묶기/수 묶기.cc
@@ -5,83 +5,81 @@ using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    int N;
-    
-    cin >> N;
-    
-    int num;
-    
-    priority_queue<int> plus;
-    priority_queue<int, vector<int>, greater<int>> minus;
-    int one = 0;
-    int zero = 0;
-    
-    for(int i = 0; i < N; i++)
-    {
-        cin >> num;
-        
-        if(num > 1)
-        {
-            plus.push(num);
-        }
-        else if(num == 1)
-        {
-            one++;
-        }
-        else if(num == 0)
-        {
-            zero++;
-        }
-        else
-        {
-            minus.push(num);
-        }
-    }
-    
-    int sum = 0;
-    
-    while(plus.size() > 1)
-    {
-        int first = plus.top();
-        plus.pop();
-        int second = plus.top();
-        plus.pop();
-        
-        sum = sum + first * second;
-    }
-    
-    if(!plus.empty())
-    {
-        sum = sum + plus.top();
-        plus.pop();
-    }
-    
-    while(minus.size() > 1)
-    {
-        int first = minus.top();
-        minus.pop();
-        int second = minus.top();
-        minus.pop();
-        
-        sum = sum + first* second;
-    }
-    
-    if(!minus.empty())
-    {
-        if(zero == 0)
-        {
-            sum = sum + minus.top();
-            minus.pop();
-        }
-    }
-    
-    sum = sum + one;
-    
-    cout << sum << "\n";
-    
-    return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	
+	cin >> N;
+
+	priority_queue<int> plus;
+	priority_queue<int, vector<int>, greater<int>> minus;
+	int one = 0;
+	int zero = 0;
+	int num;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num;
+		if (num > 1)
+		{
+			plus.push(num);
+		}
+		else if (num == 1)
+		{
+			one++;
+		}
+		else if (num == 0)
+		{
+			zero++;
+		}
+		else
+		{
+			minus.push(num);
+		}
+	}
+
+	int sum = 0;
+
+	while (plus.size() > 1)
+	{
+		int first = plus.top();
+		plus.pop();
+		int second = plus.top();
+		plus.pop();
+		
+		sum = sum + first * second;
+	}
+
+	if (!plus.empty())
+	{
+		sum = sum + plus.top();
+		plus.pop();
+	}
+
+	while (minus.size() > 1)
+	{
+		int first = minus.top();
+		minus.pop();
+		int second = minus.top();
+		minus.pop();
+
+		sum = sum + first * second;
+	}
+
+	if (!minus.empty())
+	{
+		if (zero == 0)
+		{
+			sum = sum + minus.top();
+			minus.pop();
+		}
+	}
+
+	sum = sum + one;
+
+	cout << sum << "\n";
+
+	return 0;
 }
