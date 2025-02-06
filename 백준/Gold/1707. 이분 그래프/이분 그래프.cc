ebc3264db3e6b8d03c5cb104bc6f3d0c62927cdc
Kangdo <166014987+DohyunKang@users.noreply.github.com>
2025-02-06 09:43:15 +0900

    [Gold IV] Title: 이분 그래프, Time: 204 ms, Memory: 11396 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.cc
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+static vector<vector<int>> A;
+static vector<bool> visited;
+static vector<int> check;
+static bool IsEven;
+
+void DFS(int node);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int K;
+    int V,E;
+    int s,e;
+    
+    cin >> K;
+    
+    for(int i = 0; i < K; i++)
+    {
+        cin >> V; 
+        cin >> E;
+        
+        A.resize(V+1);
+        visited.resize(V+1);
+        check.resize(V+1);
+        IsEven = true;
+        
+        for(int i = 0; i < E; i++)
+        {
+            cin >> s >> e;
+            A[s].push_back(e);
+            A[e].push_back(s);
+        }
+        
+        for(int i = 1; i <= V; i++)
+        {
+            if(IsEven)
+            {
+                DFS(i);
+            }
+            else
+            {
+                break;
+            }
+        }
+        
+        if(IsEven)
+        {
+            cout << "YES" << "\n";
+        }
+        else
+        {
+            cout << "NO" << "\n";
+        }
+        
+        for(int i = 0; i <= V; i++)
+        {
+            A[i].clear();
+            visited[i] = false;
+            check[i] = 0;
+        }
+    }
+    
+    return 0;
+}
+
+void DFS(int node)
+{
+    visited[node] = true;
+    
+    for(int i : A[node])
+    {
+        if(!visited[i])
+        {
+            check[i] = (check[node] + 1) % 2;
+            DFS(i);
+        }
+        else if(check[i] == check[node])
+        {
+            IsEven = false;
+        }
+    }
+}
